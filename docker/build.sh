e3d57f8184e8af7a0edbbd9a3bbe518363070467
Pang <aipaw@live.cn>
2021-08-07 23:47:44 +0800

    no message

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64 -t kuaifan/sdwan:manage-0.0.1 --push -f ./manage.Dockerfile .
-docker buildx build --platform linux/amd64 -t kuaifan/sdwan:work-0.0.1 --push -f ./work.Dockerfile .
+docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:manage-0.0.1 --push -f ./manage.Dockerfile .
+docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:work-0.0.1 --push -f ./work.Dockerfile .
