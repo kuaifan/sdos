0a2f63599d543cf498a3002ceb6b78229086fdca
Pang <aipaw@live.cn>
2021-11-23 08:06:39 +0800

    -t mangle

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:manage-0.0.1 --push -f ./manage.Dockerfile . &
-docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:nginx-0.0.1 --push -f ./nginx.Dockerfile . &
-docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:work-0.0.1 --push -f ./work.Dockerfile . &
+if [ "$1" = "no-cache" ]; then
+    docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:manage-0.0.1 --push -f ./manage.Dockerfile . &
+    docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:nginx-0.0.1 --push -f ./nginx.Dockerfile . &
+    docker buildx build --no-cache --platform linux/amd64 -t kuaifan/sdwan:work-0.0.1 --push -f ./work.Dockerfile . &
+else
+    docker buildx build --platform linux/amd64 -t kuaifan/sdwan:manage-0.0.1 --push -f ./manage.Dockerfile . &
+    docker buildx build --platform linux/amd64 -t kuaifan/sdwan:nginx-0.0.1 --push -f ./nginx.Dockerfile . &
+    docker buildx build --platform linux/amd64 -t kuaifan/sdwan:work-0.0.1 --push -f ./work.Dockerfile . &
+fi
 
 wait
--- install/firewall.go
+++ install/firewall.go
@@ -30,19 +30,19 @@ func iptablesFirewallTemplate(mode string) string {
 	value := ""
 	if FirewallConfig.Address == "" {
 		if strings.Contains(FirewallConfig.Protocol, "/") {
-			tcp := fmt.Sprintf("iptables {MODE} INPUT -p tcp -m state --state NEW -m tcp --dport %s -j %s", FirewallConfig.Ports, FirewallConfig.Type)
-			udp := fmt.Sprintf("iptables {MODE} INPUT -p udp -m state --state NEW -m udp --dport %s -j %s", FirewallConfig.Ports, FirewallConfig.Type)
+			tcp := fmt.Sprintf("iptables -t mangle {MODE} PREROUTING -p tcp -m state --state NEW -m tcp --dport %s -j %s", FirewallConfig.Ports, FirewallConfig.Type)
+			udp := fmt.Sprintf("iptables -t mangle {MODE} PREROUTING -p udp -m state --state NEW -m udp --dport %s -j %s", FirewallConfig.Ports, FirewallConfig.Type)
 			value = fmt.Sprintf("%s && %s", tcp, udp)
 		} else {
-			value = fmt.Sprintf("iptables {MODE} INPUT -p tcp -m state --state NEW -m %s --dport %s -j %s", FirewallConfig.Protocol, FirewallConfig.Ports, FirewallConfig.Type)
+			value = fmt.Sprintf("iptables -t mangle {MODE} PREROUTING -p tcp -m state --state NEW -m %s --dport %s -j %s", FirewallConfig.Protocol, FirewallConfig.Ports, FirewallConfig.Type)
 		}
 	} else {
 		if strings.Contains(FirewallConfig.Protocol, "/") {
-			tcp := fmt.Sprintf("iptables {MODE} INPUT -s %s -p tcp --dport %s -j %s", FirewallConfig.Address, FirewallConfig.Ports, FirewallConfig.Type)
-			udp := fmt.Sprintf("iptables {MODE} INPUT -s %s -p udp --dport %s -j %s", FirewallConfig.Address, FirewallConfig.Ports, FirewallConfig.Type)
+			tcp := fmt.Sprintf("iptables -t mangle {MODE} PREROUTING -s %s -p tcp --dport %s -j %s", FirewallConfig.Address, FirewallConfig.Ports, FirewallConfig.Type)
+			udp := fmt.Sprintf("iptables -t mangle {MODE} PREROUTING -s %s -p udp --dport %s -j %s", FirewallConfig.Address, FirewallConfig.Ports, FirewallConfig.Type)
 			value = fmt.Sprintf("%s && %s", tcp, udp)
 		} else {
-			value = fmt.Sprintf("iptables {MODE} INPUT -s %s -p %s --dport %s -j %s", FirewallConfig.Address, FirewallConfig.Protocol, FirewallConfig.Ports, FirewallConfig.Type)
+			value = fmt.Sprintf("iptables -t mangle {MODE} PREROUTING -s %s -p %s --dport %s -j %s", FirewallConfig.Address, FirewallConfig.Protocol, FirewallConfig.Ports, FirewallConfig.Type)
 		}
 	}
 	if mode == "del" {
@@ -71,15 +71,15 @@ func iptablesFirewallDel() {
 }
 
 func iptablesDefaultAccept() {
-	_, _, _ = RunCommand("-c", "iptables -D INPUT -p icmp --icmp-type any -j ACCEPT")
-	_, _, _ = RunCommand("-c", "iptables -D INPUT -s localhost -d localhost -j ACCEPT")
-	_, _, _ = RunCommand("-c", "iptables -D INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
-	_, _, _ = RunCommand("-c", "iptables -P INPUT ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -D PREROUTING -p icmp --icmp-type any -j ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -D PREROUTING -s localhost -d localhost -j ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -D PREROUTING -m state --state ESTABLISHED,RELATED -j ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -P PREROUTING ACCEPT")
 }
 
 func iptablesDefaultDrop() {
-	_, _, _ = RunCommand("-c", "iptables -A INPUT -p icmp --icmp-type any -j ACCEPT")
-	_, _, _ = RunCommand("-c", "iptables -A INPUT -s localhost -d localhost -j ACCEPT")
-	_, _, _ = RunCommand("-c", "iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
-	_, _, _ = RunCommand("-c", "iptables -P INPUT DROP")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -A PREROUTING -p icmp --icmp-type any -j ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -A PREROUTING -s localhost -d localhost -j ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -A PREROUTING -m state --state ESTABLISHED,RELATED -j ACCEPT")
+	_, _, _ = RunCommand("-c", "iptables -t mangle -P PREROUTING DROP")
 }
